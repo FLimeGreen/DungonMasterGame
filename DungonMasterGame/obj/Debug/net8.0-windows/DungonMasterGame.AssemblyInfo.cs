e7d5b15f5eac10df9bfc6955842cdc04d31c99c3
FLimeGreen <fynn.wegen@aol.com>
2025-10-19 18:23:30 +0200

    Restructure
    Restructurierung vom Datei System

--- DungonMasterGame/obj/Debug/net8.0-windows/DungonMasterGame.AssemblyInfo.cs
+++ DungonMasterGame/obj/Debug/net8.0-windows/DungonMasterGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungonMasterGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d93af86db5333867ebfde04634b5cd9a741f50f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungonMasterGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungonMasterGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
